b16d78755f609e343b10fa3011cf607bcc05f53c
Grapedia <146814365+Grapedia@users.noreply.github.com>
2023-10-18 08:38:56 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,4 +26,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = '_static/grapedia_logo.png'
 html_static_path = ['_static']
